328d86efabfa84ac7d76c39e41c0f0cc416ec7e8
nazala-mafa <thenazala@gmail.com>
2026-08-12 06:06:25 +0700

    refactor: update environment variable names to NUXT_PUBLIC_* for consistency across configuration

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 to run directly through localhost, set .env variable to
 
-> APP_URL='http://127.0.0.1:8000'
+> NUXT_PUBLIC_APP_URL='http://127.0.0.1:8000'
 
 ensure the value has same with laravel .env APP_URL
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,13 +6,13 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css', 'leaflet/dist/leaflet.css'],
   runtimeConfig: {
     public: {
-      appUrl: process.env.APP_URL,
-      uploadChunkSizeMb: process.env.UPLOAD_CHUNK_SIZE_MB
+      appUrl: process.env.NUXT_PUBLIC_APP_URL,
+      uploadChunkSizeMb: process.env.NUXT_PUBLIC_UPLOAD_CHUNK_SIZE_MB
     }
   },
   routeRules: {
-    '/files/**': { proxy: `${process.env.APP_URL}/files/**` },
-    '/api/**': { proxy: `${process.env.APP_URL}/api/**` },
+    '/files/**': { proxy: `${process.env.NUXT_PUBLIC_APP_URL}/files/**` },
+    '/api/**': { proxy: `${process.env.NUXT_PUBLIC_APP_URL}/api/**` },
   },
   typescript: {
     strict: true,
